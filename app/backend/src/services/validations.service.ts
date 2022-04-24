266a8207c47a5f1df5cf36fee97fb468fc27fa85
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-04-24 12:36:36 -0300

    :hammer_and_wrench: improve error message

--- app/backend/src/services/validations.service.ts
+++ app/backend/src/services/validations.service.ts
@@ -2,26 +2,29 @@ import * as bcryptjs from 'bcryptjs';
 import Users from '../database/models/Users';
 import Token from './token.service';
 
+const BAD_REQUEST_ERROR_MESSAGE = 'All fields must be filled'; // status code: 400
+const UNAUTHORIZED_ERROR_MESSAGE = 'Incorrect email or password'; // status code: 401
+
+const EMAIL_REGEX = /(.+)@(.+){2,}\.(.+){2,}/;
+
 class Validations {
   static emailValidation = (email: string) => {
-    const EMAIL_REGEX = /(.+)@(.+){2,}\.(.+){2,}/;
-
     if (email === '') {
-      return { code: 400, message: 'All fields must be filled' };
+      return { code: 400, message: BAD_REQUEST_ERROR_MESSAGE };
     }
 
     if (!email.match(EMAIL_REGEX)) {
-      return { code: 401, message: 'Incorrect email or password' };
+      return { code: 401, message: UNAUTHORIZED_ERROR_MESSAGE };
     }
   };
 
   static passwordValidation = (password: string) => {
     if (password === '') {
-      return { code: 400, message: 'All fields must be filled' };
+      return { code: 400, message: BAD_REQUEST_ERROR_MESSAGE };
     }
 
     if (password.length <= 6) {
-      return { code: 401, message: 'Incorrect email or password' };
+      return { code: 401, message: UNAUTHORIZED_ERROR_MESSAGE };
     }
   };
 
@@ -29,11 +32,11 @@ class Validations {
     const userData = await Users.findOne({ where: { email } });
 
     if (!userData) {
-      return { code: 401, message: 'Incorrect email or password' };
+      return { code: 401, message: UNAUTHORIZED_ERROR_MESSAGE };
     }
 
     if (!bcryptjs.compareSync(password, userData.password)) {
-      return { code: 401, message: 'Incorrect email or password' };
+      return { code: 401, message: UNAUTHORIZED_ERROR_MESSAGE };
     }
   };
 
